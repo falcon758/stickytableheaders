86cd950eedc30f49dc909d743a723ec5b71e2d60
Luís Campos <lcampos@grupopie.com>
2013-07-11 10:29:33 +0100

    StickyTableHeaders Init

---
+++ js/jquery.stickytableheaders.js
@@ -0,0 +1,160 @@
+/*! Copyright (c) 2011 by Jonas Mosbech - https://github.com/jmosbech/StickyTableHeaders
+    MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
+
+;(function ($, window, undefined) {
+	'use strict';
+
+	var pluginName = 'stickyTableHeaders';
+	var defaults = {
+			fixedOffset: 0
+		};
+
+	function Plugin (el, options) {
+		// To avoid scope issues, use 'base' instead of 'this'
+		// to reference this class from internal events and functions.
+		var base = this;
+		var height = [];
+		var distance = 0;
+
+		// Access to jQuery and DOM versions of element
+		base.$el = $(el);
+		base.el = el;
+
+		// Cache DOM refs for performance reasons
+		base.$window = $(window);
+		base.$clonedHeader = $();
+		base.$originalHeader = $();
+
+		// Keep track of state
+		base.isCloneVisible = false;
+		base.leftOffset = null;
+		base.topOffset = null;
+		
+		base.init = function () {
+			base.options = $.extend({}, defaults, options);
+
+			base.$el.each(function () {
+				var $this = $(this);
+
+				// remove padding on <table> to fix issue #7
+				$this.css('padding', 0);
+
+				$this.wrap('<div class="divTableWithFloatingHeader"></div>');
+
+				base.$originalHeader = $('thead', $this);
+				base.$clonedHeader = base.$originalHeader.clone();
+				
+				base.$originalHeader.each(function(a,b) {
+					height[a] = parseInt($(this).outerHeight(true));
+				});
+					
+				base.$clonedHeader.each(function(a,b) {
+					$(this).addClass('tableFloatingHeader');
+					$(this).css({
+					'position': 'fixed',
+					'top': 0,
+					'z-index': 1, // #18: opacity bug
+					'margin-top': distance,
+					'display': 'none'
+					});
+					distance += height[a];
+				});
+
+				base.$originalHeader.each(function() {
+					$(this).addClass('tableFloatingHeaderOriginal');
+				});
+				base.$originalHeader.each(function(a,b) {
+					$(this).after(base.$clonedHeader[a]);
+				});
+
+				// enabling support for jquery.tablesorter plugin
+				// forward clicks on clone to original
+				base.$clonedHeader.each(function(a,b) {
+					var $thisone = $(this);
+					$('th', this).click(function (e) {
+						var index = $('th', $thisone).index(this);
+						$('th', base.$originalHeader[a]).eq(index).click();
+					});
+				});
+				$this.bind('sortEnd', base.updateWidth);
+			});
+
+			base.updateWidth();
+			base.toggleHeaders();
+
+			base.$window.scroll(base.toggleHeaders);
+			base.$window.resize(base.toggleHeaders);
+			base.$window.resize(base.updateWidth);
+		};
+
+		base.toggleHeaders = function () {
+			base.$el.each(function () {
+				var $this = $(this);
+
+				var newTopOffset = isNaN(base.options.fixedOffset) ?
+					base.options.fixedOffset.height() : base.options.fixedOffset;
+
+				var offset = $this.offset();
+				var scrollTop = base.$window.scrollTop() + newTopOffset;
+				var scrollLeft = base.$window.scrollLeft();
+
+				if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height())) {
+					var newLeft = offset.left - scrollLeft;
+					if (base.isCloneVisible && (newLeft === base.leftOffset) && (newTopOffset === base.topOffset)) {
+						return;
+					}				
+					base.$clonedHeader.each(function() {
+						$(this).css({
+						'top': newTopOffset,
+						'left': newLeft,
+						'display': 'block'
+						});
+					});
+					base.$originalHeader.each(function() {
+						$(this).css('visibility', 'hidden');
+					});
+					base.isCloneVisible = true;
+					base.leftOffset = newLeft;
+					base.topOffset = newTopOffset;
+				}
+				else if (base.isCloneVisible) {
+					base.$clonedHeader.each(function() {
+						$(this).css('display', 'none');
+					});
+					base.$originalHeader.each(function() {
+						$(this).css('visibility', 'visible');
+					});
+					base.isCloneVisible = false;
+				}
+			});
+		};
+
+		base.updateWidth = function () {
+			// Copy cell widths and classes from original header
+			base.$clonedHeader.each(function(a,b) {
+				$('th', this).each(function (index) {
+					var $this = $(this);
+					var $origCell = $('th', base.$originalHeader[a]).eq(index);
+					$this.className = $origCell.attr('class') || '';
+					$this.css('width', $origCell.width());
+				});
+			});
+			// Copy row width from whole table
+			base.$clonedHeader.css('width', base.$originalHeader.width());
+		};
+
+		// Run initializer
+		base.init();
+	}
+
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[pluginName] = function ( options ) {
+		return this.each(function () {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+			}
+		});
+	};
+
+})(jQuery, window);
---
+++ readme.md
@@ -0,0 +1,35 @@
+StickyTableHeaders
+==================
+So what's it good for? Well, let's say you want to display a long list of fairly uniform tabluar data, like [stock exchange listings](http://online.barrons.com/public/page/majormarket-nysecomposite-A.html) or [sport statistics](http://sports.yahoo.com/nba/stats/byposition?pos=PG,SG,G,GF,SF,PF,F,FC,C) but you don't want your users to get lost in the data as they scroll down on the page.
+
+StickyTableHeaders to the rescue: By applying the StickyTableHeaders jQuery plugin to the table, the column headers will stick to the top of the viewport as you scroll down.
+
+Go ahead and [try out a demo](http://jsfiddle.net/jmosbech/stFcx/).
+
+The code is based on [this proof of concept](http://stackoverflow.com/questions/1030043/html-table-headers-always-visible-at-top-of-window-when-viewing-a-large-table/1041566#1041566).
+
+Future Work
+-----------
+-   Writing a better readme :)
+-   Providing a minified version
+
+
+Known Issues
+------------
+-   You need to set the padding of the `<th>`s explicitly in the css in order to make the plugin work in Internet Explorer
+-   Internet Explorer: Adding horizontal margin to the table causes the header to be misaligned when scrolling. (Issue #10)
+-   `border-collapse: collapse` is not supported (issue #2)
+-   Using the plugin together with [tablesorter](http://tablesorter.com/docs/) breaks in Internet Explorer 8
+
+
+Browser Support
+---------------
+The plugin has been verified to work in:
+
+-   Chrome 20
+-   Firefox 13
+-   Internet Explorer 9
+-   Internet Explorer 8
+-   Safari 5
+
+NOTE: It does not currently work in Internet Explorer 7 (but it degrades nicely)
